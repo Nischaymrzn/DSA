3cd8bc354a8da8f390e511daabbbe2be4083840e
Nischaymrzn <nischaymaharjann@gmail.com>
2024-11-21 22:43:18 +0545

    feat: day 2

---
+++ DataStructures/Stacks.java
@@ -0,0 +1,62 @@
+package DataStructures;
+
+//stack implementation from scratch
+public class Stacks {
+    int[] stk;
+    int top;
+    int size;
+
+    Stacks(int size){
+        this.size=size;
+        top=-1;
+        stk=new int[size];
+    }
+
+    void push(int data){
+        if(this.isFull()){
+            System.out.println("Stack Overflow!");
+            return;
+        }
+        stk[++top]=data;
+        System.out.println("Added element "+this.stk[top]);
+    }
+
+    int pop(){
+        if(this.isEmpty()){
+            System.out.println("Stack is empty!");
+            return -1;
+        }
+        
+        return stk[top--];
+    }
+
+    int peek(){
+        if(this.isEmpty()){
+            System.out.println("Stack is empty!");
+            return -1;
+        }
+        return stk[top];
+    }
+
+    public boolean isEmpty(){
+        return top==-1;
+    }
+
+    public boolean isFull(){
+        return top==size-1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Stack Implementation");
+        Stacks newStack=new Stacks(4);
+        newStack.push(1);
+        newStack.push(2);
+        System.out.println("The top element is "+newStack.peek());
+        newStack.push(3);
+        newStack.push(4);
+        newStack.push(5);
+        System.out.println("Popped element"+newStack.pop());
+        System.out.println("The top element is "+newStack.peek());
+
+    }
+}
---
+++ Leetcode/EvaluateReversePolishNotation_015.java
@@ -0,0 +1,81 @@
+package Leetcode;
+
+// 150. Evaluate Reverse Polish Notation
+
+import java.util.Stack;
+
+// Medium
+// Topics
+// Companies
+// You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+// Evaluate the expression. Return an integer that represents the value of the expression.
+// Note that:
+// The valid operators are '+', '-', '*', and '/'.
+// Each operand may be an integer or another expression.
+// The division between two integers always truncates toward zero.
+// There will not be any division by zero.
+// The input represents a valid arithmetic expression in a reverse polish notation.
+// The answer and all the intermediate calculations can be represented in a 32-bit integer.
+// Example 1:
+// Input: tokens = ["2","1","+","3","*"]
+// Output: 9
+// Explanation: ((2 + 1) * 3) = 9
+// Example 2:
+// Input: tokens = ["4","13","5","/","+"]
+// Output: 6
+// Explanation: (4 + (13 / 5)) = 6
+// Example 3:
+// Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+// Output: 22
+// Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+// = ((10 * (6 / (12 * -11))) + 17) + 5
+// = ((10 * (6 / -132)) + 17) + 5
+// = ((10 * 0) + 17) + 5
+// = (0 + 17) + 5
+// = 17 + 5
+// = 22
+ 
+
+public class EvaluateReversePolishNotation_015 {
+    public int evalRPN(String[] tokens) {
+             Stack<Integer> stk = new Stack<>();
+            for (String token : tokens) {
+                if (token.equals("+")) {
+                    int b = stk.pop();
+                    int a = stk.pop();
+                    stk.push(a + b);
+                } else if (token.equals("-")) {
+                    int b = stk.pop();
+                    int a = stk.pop();
+                    stk.push(a - b);
+                } else if (token.equals("*")) {
+                    int b = stk.pop();
+                    int a = stk.pop();
+                    stk.push(a * b);
+                } else if (token.equals("/")) {
+                    int b = stk.pop();
+                    int a = stk.pop();
+                    stk.push(a / b);
+                } else {
+                    // It's a number
+                    stk.push(Integer.parseInt(token));
+                }
+            }
+            // The final result will be the only element in the stack
+            return stk.pop();
+    
+            
+            
+        } 
+        
+    
+
+    
+    public static void main(String[] args) {
+        EvaluateReversePolishNotation_015 n=new EvaluateReversePolishNotation_015();
+        String[] input = new String[] {"2", "1", "+", "3", "*"};
+        System.out.println(n.evalRPN(input));
+        
+        
+    }
+}
