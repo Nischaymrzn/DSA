aa0db5b4aae67c09a6cf657d35f1b32522d431a5
Nischaymrzn <nischaymaharjann@gmail.com>
2025-01-01 20:09:04 +0545

    Permutation problem

--- Algorithms/Calculate_Permutation.java
+++ Algorithms/Calculate_Permutation.java
@@ -3,14 +3,14 @@ package algorithms;
 public class Calculate_Permutation {
 
     void findPermutation(String str, String perm){
-        if(str.isEmpty()){
+        if(str.isEmpty()){  //base case -> return if string is empty
             System.out.println(perm);
             return;
         }
         for (int i=0;i<str.length();i++){
             char curChar=str.charAt(i);
             String remStr=str.substring(0,i)+str.substring(i+1);
-            findPermutation(remStr, perm+curChar);
+            findPermutation(remStr, perm+curChar);  //recursion case -> call the same function with argument, str without the curr character , and perm by adding current char to it
 
         }
 
@@ -19,8 +19,8 @@ public class Calculate_Permutation {
     public static void main(String[] args) {
         Calculate_Permutation p=new Calculate_Permutation();
         String str="ABC";
-        String perm=" ";
+        String perm="";
+        System.out.println("Permutation of string '" + str + "' are: ");
         p.findPermutation(str, perm);
-        System.out.println("Hello");
     }
 }
